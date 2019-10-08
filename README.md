070b420d1bfa52c9b30b92f121b9680a243d44a4
Gavin lyons <glyons66@hotmail.com>
2019-10-08 01:37:50 +0100

    Version 1.1

--- README.md
+++ README.md
@@ -3,59 +3,76 @@ Overview
 * Name: opml_convert_RSS
 * Description: Command line python utility , converts opml(xml) to RSS.
 * Author: Gavin Lyons.
+* Software: python 3.6.8
 
+Table of contents
+---------------------------
 
+  * [Overview](#overview)
+  * [Table of contents](#table-of-contents)
+  * [Description](#Description)
+  * [Usage](#usage)
+  * [Copyright](#copyright)
+  
 Description
 ---------
 
 Command line python utility, 
 Converts the opml(Xml) file of a youtube user subscriptions provided by youtube, 
-into a text file which can added to RSS reader setup file 
+into a text file which can added to RSS reader setup file
 file, like newsboat for example. Thus converting your youtube subscriptions,
-into a group of tagged RSS feeds.
+into a group of tagged RSS feeds. 
 Example input xml and output txt files are available in Documents folder
+The user can select input file path, output file path and custom tag.
 
 **Input:**
 
-opml(xml) file provided by user on comand prompt using -f option.
+opml(xml) file provided by user on command prompt using -i option.
 
 **Output:**
 
-Output.txt file in following format:
+The output.txt is file is provided by user using -o option 
+If no file is provided the program uses a default file called output.txt at path of
+program. The output file is in file in following format:
 
 [url] ~"[video_title]" [tag]
 
 https://www.youtube.com/feeds/videos.xml?channel_id=UCI999ua  "~Python Training by Bader"  youtube
 
-Where tag = youtube
+Where tag default = youtube, A custom tag can be added using -t option.
 
-usage
+Usage
 --------
 
-1. Download the python program, youtube_sub_convert.py.
+1. Download the python program, opml_convert_RSS.py.
 2. In a web browser navigate to subscription manager page of youtube.
 https://www.youtube.com/subscription_manager
 3. At bottom of page click on "Export to RSS readers -- Export subscriptions".
 4. This subscription_manager file will be downloaded into an opml(xml) file.
-5. Run program in terminal. $ python3 youtube_sub_convert.py -f opmlfilename
-6. A file called output.txt will be created at path of program. 
-7. Copy this into RSS reader setup files.
+5. Run program in terminal. 
+	* $ python3 opml_convert_RSS.py -i infile -o outfile -t tech
+	* -i infile is the full path and/or filename of input xml file.
+	* -0 outfile is the full path and/or filename of output txt file.
+	* -t tech is a custom tag.
+	* -t and -o options can be omitted and defaults used if user wants.
+	
+![SS](https://raw.githubusercontent.com/gavinlyonsrepo/opml_convert_RSS/master/screenshots/opml.png)
+
+7. Copy the output txt file into RSS reader setup files(eg newsboat)
  
  
-Arguments
--------------
+**Arguments**
 
-usage: youtube_sub_convert.py [-h] [-v] [-f FILE]
+usage: opml_convert_rss.py [-h] [-v] [-i INFILE] [-o OUTFILE] [-t TAGNAME]
 
-URL help at: 
-
-https://github.com/gavinlyonsrepo/opml_convert_RSS
+URL help at: https://github.com/gavinlyonsrepo/opml_convert_RSS
 
 optional arguments:
-
-1.  -h, --help  show  help message and exit
-2.  -v          Print version and quit
-3.   -f FILE     inputfile xml file , fullpath and/or filename
+  -h, --help  show this help message and exit
+  -v          Print version and quit
+  -i INFILE   input filename and/or full path
+  -o OUTFILE  output filename and/or full path, default ./outfile.txt
+  -t TAGNAME  tagname, default = youtube
 
 
 Copyright
---
+++ code/opml_convert_rss.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+"""python script to turn youtube opml sub file into text file for rss reader"""
+# =======================HEADER=======================================
+# title             : youtube_sub_convert.py
+# description       : python script to turn youtube
+# opml sub file into text file for rss reader newsboat.
+# author            : Gavin Lyons
+# date              : OCT  2019
+# version           : 1.1
+# web               : See __URL__
+# mail              : glyons66@hotmail.com
+# python_version    : 3.6.8
+
+# ==========================IMPORTS======================
+# Import the system modules needed to run
+from xml.etree import ElementTree
+import argparse
+
+# =============Functions==============
+# metadata
+__VERSION__ = "1.1"
+__URL__ = "https://github.com/gavinlyonsrepo/opml_convert_RSS"
+
+
+# ================== FUNCTIONS ===============================
+def opml_txt(text):
+    """ function to turn opml(xml) file into textfile two parts
+    parse the input and create output"""
+    msg_func("bold", "Start in main")
+    urls = []
+    texts = []
+    infilename, outfilename, tagname = process_cmd_arguments()
+
+#  Parse the input file, Stage 1
+    try:
+        with open(infilename, 'rt') as inputfile:
+            tree = ElementTree.parse(inputfile)
+        for node in tree.findall('.//outline'):
+            url = node.attrib.get('xmlUrl')
+            if url:
+                urls.append(url)
+
+        for node in tree.findall('.//outline'):
+            text = node.attrib.get('text')
+            if text:
+                texts.append(text)
+
+        texts.pop(0)
+
+    except Exception as error:
+        msg_func("red", "Error: with input file:  {} = {}".format(infilename, error))
+        quit()
+    else:
+        print("Input file {} parsing completed.".format(infilename))
+
+
+# Create the output file , Stage 2
+    try:
+        with open(outfilename, "w") as myoutfile:
+            for i, j in zip(urls, texts):
+                myoutfile.write('{0}  "~{1}"  {2}'.format(i, j, tagname) + '\n')
+    except Exception as error:
+        msg_func("red", "Error: With output file: {} = {}".format(outfilename, error))
+        quit()
+    else:
+        print("Output file {} created. tagname {}.".format(outfilename, tagname))
+
+
+def process_cmd_arguments():
+    """Parse the command line arguments"""
+    str_desc = "URL help at: {}".format(__URL__)
+    parser = argparse.ArgumentParser(description=str_desc)
+    parser.add_argument('-v', help='Print version and quit', default=False,
+                        dest='version', action='store_true')
+    parser.add_argument('-i', help='input filename and/or full path',
+                        type=str, dest='infile')
+    parser.add_argument('-o', help='output filename and/or full path, default ./outfile.txt',
+                        type=str, default='outfile.txt', dest='outfile')
+    parser.add_argument('-t', help='tagname,  default = youtube',
+                        type=str, default='youtube', dest='tagname')
+    args = parser.parse_args()
+    if args.version:
+        msg_func("bold", "Version: " + __VERSION__)
+        msg_func("bold", "Endex")
+        msg_func("line", "")
+        quit()
+
+    if args.infile:
+        infilename = args.infile
+    else:
+        msg_func("red", "Error: No input file given")
+        quit()
+
+    if args.tagname:
+        tagname = args.tagname
+
+    if args.outfile:
+        outfilename = args.outfile
+
+    return infilename, outfilename, tagname
+
+
+def msg_func(myprocess, mytext):
+    """NAME : msg_func
+    DESCRIPTION :prints to screen
+    prints line, text and anykey prompts,
+    INPUTS : $1 process name $2 text input
+    PROCESS :[1]  print line 
+    [2] print text  "green , red ,blue "
+    """
+
+    # colours for print
+    blue = '\033[94m'
+    red = '\033[91m'
+    bold = '\033[1m'
+    end = '\033[0m'
+
+    if myprocess == "line":  # print blue horizontal line of =
+        print(blue + "="*80 + end)
+
+    if myprocess == "red":  # print red text
+        print(red + mytext + end)
+
+    if myprocess == "blue":  # print blue text
+        print(blue + mytext + end)
+
+    if myprocess == "bold":  # print bold text
+        print(bold + mytext + end)
+
+
+# =====================MAIN===============================
+if __name__ == '__main__':
+    msg_func("line", "")
+    opml_txt("Start")
+    msg_func("bold", "Endex")
+    msg_func("line", "")
+else:
+    opml_txt("Imported {}".format(__name__))
+# =====================END===============================
